3b128c74c893faa98180e9da8fe5e31bccaaafd7
Oliwier Juncewicz <oli.junc@gmail.com>
2025-06-22 08:27:42 +0200

    Added navigation logic to main.js. Deleted visibility toggling (using opacity in CSS now). Added a href to each of artists name for navigation. Started working on displayArtist

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+const state = {
+  currentPage: window.location.pathname,
+};
+
 const tracksContainer = document.getElementById('tracks-container');
 const form = document.getElementById('form');
 
@@ -5,6 +9,96 @@ async function getToken() {
   const res = await fetch('/.netlify/functions/getToken');
   const data = await res.json();
   return data.access_token;
+}
+
+function createTrackCard(track) {
+  const row = document.createElement('div');
+  row.classList.add('track-row');
+
+  const imgContainer = document.createElement('div');
+  imgContainer.style.position = 'relative';
+
+  const img = document.createElement('img');
+  img.classList.add('cover-img');
+  img.src = track.album.images[2]?.url || './media/default-cover.png';
+  img.alt = track.name ? `${track.name} cover` : 'cover';
+  img.style.display = 'block';
+
+  const play = document.createElement('a');
+  play.classList.add('play-btn');
+  play.style.position = 'absolute';
+  play.style.top = '45%';
+  play.style.left = '40%';
+  play.style.transform = 'translate(-50%, -50%)';
+  play.href = track.external_urls.spotify;
+  play.target = '_blank';
+
+  const playIcon = document.createElement('i');
+  playIcon.classList.add('fa-solid', 'fa-play', 'play-icon');
+
+  const info = document.createElement('div');
+  info.classList.add('track-info');
+
+  const title = document.createElement('h3');
+  title.textContent = track.name || 'track name';
+
+  const artistsContainer = document.createElement('div');
+
+  track.artists.forEach((artist, index) => {
+    const artistText = document.createElement('a');
+    artistText.textContent = artist.name || 'artist';
+    artistText.href = `/artist.html?id=${artist.id}`;
+    artistText.classList.add('artist');
+    artistsContainer.appendChild(artistText);
+
+    if (index < track.artists.length - 1) {
+      const comma = document.createElement('span');
+      comma.classList.add('comma');
+      comma.textContent = ',';
+      comma.style.color = '#bbbbbb';
+      artistsContainer.appendChild(comma);
+    }
+  });
+
+  info.appendChild(title);
+  info.appendChild(artistsContainer);
+
+  const save = document.createElement('button');
+  save.classList.add('save-btn');
+
+  const saveIcon = document.createElement('i');
+  saveIcon.classList.add('fa-solid', 'fa-circle-plus', 'scalable');
+
+  saveIcon.addEventListener('click', () => {
+    const isFavorite = saveIcon.classList.toggle('isFavorite');
+    if (isFavorite) {
+      saveIcon.classList.replace('fa-circle-plus', 'fa-circle-check');
+    } else {
+      saveIcon.classList.replace('fa-circle-check', 'fa-circle-plus');
+    }
+  });
+
+  const duration = document.createElement('span');
+  duration.textContent = convertMsToMinutes(track.duration_ms);
+
+  const elipsis = document.createElement('button');
+  elipsis.classList.add('elipsis-btn');
+
+  const elipsisIcon = document.createElement('i');
+  elipsisIcon.classList.add('fa-solid', 'fa-ellipsis', 'scalable');
+
+  play.appendChild(playIcon);
+  save.appendChild(saveIcon);
+  elipsis.appendChild(elipsisIcon);
+  imgContainer.appendChild(img);
+  imgContainer.appendChild(play);
+  row.appendChild(imgContainer);
+  row.appendChild(info);
+  row.appendChild(save);
+  row.appendChild(duration);
+  row.appendChild(elipsis);
+
+  return row;
 }
 
 async function searchSong(e) {
@@ -40,113 +134,12 @@ async function searchSong(e) {
   });
 }
 
-function createTrackCard(track) {
-  const row = document.createElement('div');
-  row.classList.add('track-row');
-
-  const imgContainer = document.createElement('div');
-  imgContainer.style.position = 'relative';
-
-  const img = document.createElement('img');
-  img.src = track.album.images[2]?.url || './media/default-cover.png';
-  img.alt = track.name ? `${track.name} cover` : 'cover';
-  img.style.display = 'block';
-
-  const play = document.createElement('a');
-  play.classList.add('play-btn');
-  play.style.visibility = 'hidden';
-  play.style.position = 'absolute';
-  play.style.top = '45%';
-  play.style.left = '40%';
-  play.style.transform = 'translate(-50%, -50%)';
-  play.href = track.external_urls.spotify;
-  play.target = '_blank';
-
-  const playIcon = document.createElement('i');
-  playIcon.classList.add('fa-solid', 'fa-play', 'play-icon');
-
-  const info = document.createElement('div');
-  info.classList.add('track-info');
-
-  const title = document.createElement('h3');
-  title.textContent = track.name || 'track name';
-
-  const artistsContainer = document.createElement('div');
-
-  track.artists.forEach((artist, index) => {
-    const artistText = document.createElement('a');
-    artistText.textContent = artist.name || 'artist';
-    artistText.href = artist.external_urls.spotify;
-    artistText.classList.add('artist');
-    artistsContainer.appendChild(artistText);
-
-    if (index < track.artists.length - 1) {
-      const comma = document.createElement('span');
-      comma.classList.add('comma');
-      comma.textContent = ',';
-      comma.style.color = '#bbbbbb';
-      artistsContainer.appendChild(comma);
-    }
-  });
-
-  info.appendChild(title);
-  info.appendChild(artistsContainer);
-
-  const save = document.createElement('button');
-  save.classList.add('save-btn');
-
-  const saveIcon = document.createElement('i');
-  saveIcon.classList.add('fa-solid', 'fa-circle-plus', 'scalable');
-  saveIcon.style.visibility = 'hidden';
-
-  saveIcon.addEventListener('click', () => {
-    const isFavorite = saveIcon.classList.toggle('isFavorite');
-    if (isFavorite) {
-      saveIcon.classList.replace('fa-circle-plus', 'fa-circle-check');
-    } else {
-      saveIcon.classList.replace('fa-circle-check', 'fa-circle-plus');
-    }
-  });
-
-  const duration = document.createElement('span');
-  duration.textContent = convertMsToMinutes(track.duration_ms);
-
-  const elipsis = document.createElement('button');
-  elipsis.classList.add('elipsis-btn');
-
-  const elipsisIcon = document.createElement('i');
-  elipsisIcon.classList.add('fa-solid', 'fa-ellipsis', 'scalable');
-  elipsisIcon.style.visibility = 'hidden';
-
-  play.appendChild(playIcon);
-  save.appendChild(saveIcon);
-  elipsis.appendChild(elipsisIcon);
-  imgContainer.appendChild(img);
-  imgContainer.appendChild(play);
-  row.appendChild(imgContainer);
-  row.appendChild(info);
-  row.appendChild(save);
-  row.appendChild(duration);
-  row.appendChild(elipsis);
-
-  row.addEventListener('mouseenter', () => {
-    saveIcon.style.visibility = 'visible';
-    elipsisIcon.style.visibility = 'visible';
-    img.style.opacity = '0.5';
-    play.style.visibility = 'visible';
-  });
-
-  row.addEventListener('mouseleave', () => {
-    saveIcon.style.visibility = 'hidden';
-    elipsisIcon.style.visibility = 'hidden';
-    img.style.opacity = '1';
-    play.style.visibility = 'hidden';
-  });
-
-  return row;
+async function displayArtist() {
+  const params = new URLSearchParams(window.location.search)
+  const artistId = params.get('id');
+  console.log(artistId);
 }
 
-const onRowClick = () => {};
 
 function convertMsToMinutes(ms) {
   if (!ms) return 'duration';
@@ -156,7 +149,14 @@ function convertMsToMinutes(ms) {
 }
 
 const init = () => {
-  form.addEventListener('submit', searchSong);
+  const page = state.currentPage;
+  if (page.endsWith('/') || page.endsWith('/index.html')) {
+    form.addEventListener('submit', searchSong);
+  } else if (page.endsWith('/artist.html')) {
+    displayArtist()
+  } else {
+    console.warn('Page not recognized:', page);
+  }
 };
 
 init();
