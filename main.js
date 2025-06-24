93f089e8e2c0ce931f9597fb2744a8c603c1377a
Oliwier Juncewicz <oli.junc@gmail.com>
2025-06-24 06:45:09 +0200

    Added a function to create the best result card. Added ordered list-ish numbering for the popular tracks. Added the popular tracks in artist.html. Container in the artists page to be fixed

--- main.js
+++ main.js
@@ -10,9 +10,71 @@ async function getToken() {
   return data.access_token;
 }
 
-function createTrackCard(track) {
+function createBestResultCard(track) {
+  const bestTrackRow = document.createElement('div');
+  bestTrackRow.classList.add('best-track-card');
+
+  const img = document.createElement('img');
+  img.classList.add('cover-img');
+  img.src = track.album.images[0]?.url || './media/default-cover.png';
+  img.alt = track.name ? `${track.name} cover` : 'Cover';
+  img.style.display = 'block';
+
+  // const play = document.createElement('a');
+  // play.classList.add('play-btn');
+  // play.style.position = 'absolute';
+  // play.style.top = '45%';
+  // play.style.left = '40%';
+  // play.style.transform = 'translate(-50%, -50%)';
+  // play.href = track.external_urls.spotify;
+  // play.target = '_blank';
+
+  // const playIcon = document.createElement('i');
+  // playIcon.classList.add('fa-solid', 'fa-play', 'play-icon');
+
+  const info = document.createElement('div');
+  info.classList.add('best-track-info');
+
+  const title = document.createElement('h3');
+  title.textContent = track.name || 'track name';
+
+  const artistsContainer = document.createElement('div');
+
+  track.artists.forEach((artist, index) => {
+    const artistText = document.createElement('a');
+    artistText.textContent = artist.name || 'artist';
+    artistText.href = `/artist.html?id=${artist.id}`;
+    artistText.classList.add('artist');
+    artistsContainer.appendChild(artistText);
+
+    if (index < track.artists.length - 1) {
+      const comma = document.createElement('span');
+      comma.classList.add('comma');
+      comma.textContent = ',';
+      comma.style.color = '#bbbbbb';
+      artistsContainer.appendChild(comma);
+    }
+  });
+
+  info.appendChild(title);
+  info.appendChild(artistsContainer);
+
+  bestTrackRow.appendChild(img);
+  bestTrackRow.appendChild(info);
+
+  return bestTrackRow;
+}
+
+function createTrackCard(track, number = null) {
   const row = document.createElement('div');
   row.classList.add('track-row');
+
+  if (number !== null) {
+    const listNumber = document.createElement('p');
+    listNumber.classList.add('list-number');
+    listNumber.textContent = number;
+    row.appendChild(listNumber);
+  }
 
   const imgContainer = document.createElement('div');
   imgContainer.style.position = 'relative';
@@ -104,6 +166,7 @@ async function searchSong(e) {
   e.preventDefault();
 
   const tracksContainer = document.getElementById('tracks-container');
+  const bestTrackContainer = document.getElementById('best-track-container');
   const searchField = document.getElementById('search');
   const query = searchField.value;
   const token = await getToken();
@@ -121,6 +184,7 @@ async function searchSong(e) {
   const data = await res.json();
   const tracks = data.tracks.items;
 
+  bestTrackContainer.innerHTML = '';
   tracksContainer.innerHTML = '';
 
   if (tracks.length === 0) {
@@ -128,9 +192,16 @@ async function searchSong(e) {
     return;
   }
 
+  const bestTrackHeader = document.createElement('h2');
+  bestTrackHeader.textContent = 'Best result';
+  bestTrackContainer.appendChild(bestTrackHeader);
+
   const tracksHeader = document.createElement('h2');
   tracksHeader.textContent = 'Tracks';
   tracksContainer.appendChild(tracksHeader);
+
+  const bestCard = createBestResultCard(tracks[0]);
+  bestTrackContainer.appendChild(bestCard);
 
   tracks.forEach((track) => {
     const card = createTrackCard(track);
@@ -139,6 +210,10 @@ async function searchSong(e) {
 }
 
 async function displayArtist() {
+  const popularTracksContainer = document.getElementById(
+    'popular-tracks-container'
+  );
+
   const params = new URLSearchParams(window.location.search);
   const artistId = params.get('id');
 
@@ -151,11 +226,20 @@ async function displayArtist() {
       },
     }
   );
+  const topTracksRes = await fetch(
+    `${BASE_URL}artists/${encodeURIComponent(artistId)}/top-tracks`,
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
 
   const data = await res.json();
+  const topTracksData = await topTracksRes.json();
+  const tracks = topTracksData.tracks;
 
   const spotifyCard = document.querySelector('.spotify-card');
-  console.log(spotifyCard);
 
   const hero = document.createElement('div');
   const bgImage = data.images?.[0]?.url;
@@ -219,6 +303,13 @@ async function displayArtist() {
 
   spotifyCard.appendChild(hero);
   spotifyCard.appendChild(popularHeader);
+
+  popularTracksContainer.appendChild(popularHeader);
+  tracks.forEach((track, index) => {
+    const card = createTrackCard(track, index + 1);
+    popularTracksContainer.appendChild(card);
+  });
+  spotifyCard.appendChild(popularTracksContainer);
 }
 
 function convertMsToMinutes(ms) {
@@ -229,15 +320,15 @@ function convertMsToMinutes(ms) {
 }
 
 const init = () => {
-  const page = state.currentPage;
-  if (page.endsWith('/') || page.endsWith('/index.html')) {
-    const form = document.getElementById('form');
-    if (!form) return;
+  const form = document.getElementById('form');
+  if (form) {
     form.addEventListener('submit', searchSong);
-  } else if (page.endsWith('/artist.html')) {
+  }
+
+  if (state.currentPage.endsWith('/artist.html')) {
     displayArtist();
   } else {
-    console.warn('Page not recognized:', page);
+    console.warn('Page not recognized:', state.currentPage);
   }
 };
 
